0110a1421077750075c14d098ae98bf978de8ec4
Nixon00013 <nixon.dev.com@gmail.com>
2021-07-22 20:49:04 +0300

    v2

--- bulygin.js
+++ bulygin.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+function create_newUser() {
+    let newUser = {
+        'firstName': '',
+        'lastName': '',
+
+    };
+
+    newUser.firstName = prompt('Введите имя').substr(0, 1).toLowerCase();
+    newUser.lastName = prompt('Введите Фамилию').toLowerCase();
+    newUser.login = newUser.firstName + newUser.lastName;
+    console.log(newUser.login);
+}
+
+create_newUser();
